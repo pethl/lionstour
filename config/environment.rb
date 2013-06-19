683f5c6c87b6d38ae6e41deba5036c2d25b16139
pethl <admin@fencedweller.com>
2013-06-19 12:08:15 +0100

    removed prod stmp settings

--- config/environment.rb
+++ config/environment.rb
@@ -6,12 +6,4 @@ Lions::Application.initialize!
 
 Date::DATE_FORMATS.merge!(:default => "%d/%m/%Y")
 
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['app16242298@heroku.com'],
-  :password       => ENV['jf7myg5b'],
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
+
