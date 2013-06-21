16a682b2903a458a4cad350004936b5cf9840405
pethl <admin@fencedweller.com>
2013-06-21 09:05:36 +0100

    coorected user visibility

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [ :show] 
-  before_filter :correct_user,   only: [:edit, :update]
+
+  before_filter :correct_user,   only: [:show, :edit, :update]
   before_filter :admin_user,     only: [:index]
   # GET /users
   # GET /users.json
